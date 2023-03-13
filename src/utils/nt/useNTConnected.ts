d787c6eaaff868d03ba7f5f3d82a0b08dd33eaa8
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-13 13:01:38 -0400

    copy ntcore-react into project

---
+++ src/utils/nt/useNTConnected.ts
@@ -0,0 +1,24 @@
+import { useContext, useEffect, useState } from "react";
+
+import { NTContext } from "./NTProvider";
+
+const useNTConnected = () => {
+  const client = useContext(NTContext);
+  const [connected, setConnected] = useState(false);
+  useEffect(() => {
+    if (client) {
+      const cleanup = client.addRobotConnectionListener((connected) => {
+        setConnected(connected);
+      });
+      return () => {
+        if (cleanup) {
+          cleanup();
+        }
+      };
+    }
+  }, [client]);
+
+  return connected;
+};
+
+export default useNTConnected;
---
+++ src/utils/nt/useNTValue.ts
@@ -0,0 +1,39 @@
+import { NetworkTablesTypeInfo } from "ntcore-ts-client";
+import { useContext, useEffect, useState } from "react";
+
+import { NTTopicTypes } from "@/types/nt";
+
+import { NTContext } from "./NTProvider";
+
+const useNTValue = <T extends NTTopicTypes>(
+  key: string,
+  ntType: NetworkTablesTypeInfo,
+  defaultValue: T
+) => {
+  const client = useContext(NTContext);
+  const [value, setValue] = useState<T>(defaultValue);
+
+  useEffect(() => {
+    if (client) {
+      const listener = (value: T | null) => {
+        setValue(value ?? defaultValue);
+      };
+      const clientTopic = client.createTopic(key, ntType, defaultValue);
+      const subscriptionUID = clientTopic.subscribe(listener);
+
+      return () => {
+        if (subscriptionUID && clientTopic) {
+          clientTopic.unsubscribe(subscriptionUID);
+        }
+      };
+    } else {
+      throw new Error(
+        "No NTProvider found. Please wrap your application in an NTProvider"
+      );
+    }
+  }, [key, client]);
+
+  return value;
+};
+
+export default useNTValue;
