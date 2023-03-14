df169839fec425149e7a40a42951358593d057b5
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-14 15:24:51 -0400

    type eslint config & add ignore rule for storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,35 @@
 // eslint-disable-next-line no-undef
+/** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "eslint-plugin-react-refresh"],
   settings: {
     react: {
-      version: "detect"
-    }
+      version: "detect",
+    },
   },
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   rules: {
     "react-refresh/only-export-components": "warn",
-    "no-undef": "warn"
-  }
+    "no-undef": "warn",
+  },
+  overrides: [
+    {
+      files: ["*.stories.tsx"],
+      rules: {
+        "react-refresh/only-export-components": "off",
+      },
+    },
+  ],
 };
