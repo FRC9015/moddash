905872ea9546a20f775d49e6445b37dd24146a92
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-14 12:21:53 -0400

    add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,19 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "prettier",
-  ],
-
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "prettier", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "eslint-plugin-react-refresh"],
-
   settings: {
     react: {
-      version: "detect",
-    },
+      version: "detect"
+    }
   },
-
   env: {
     browser: true,
-    node: true,
+    node: true
   },
-
   rules: {
     "react-refresh/only-export-components": "warn",
-    "no-undef": "warn",
-  },
+    "no-undef": "warn"
+  }
 };
