fef6cd0d8b615ea9ea867ed4626fa9392bc5d4c9
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-04-17 13:29:35 -0400

    render buttons for tabs

---
+++ src/components/Tabs/TabButtons.tsx
@@ -0,0 +1,26 @@
+import { Link } from "@swan-io/chicane";
+import clsx from "clsx";
+import { FC } from "react";
+
+import { router } from "@/router";
+import { useMDTabs } from "@/utils/moddash/useMDTabs";
+
+export const TabButtons: FC = () => {
+  const tabs = useMDTabs();
+  console.log({ tabs });
+  return (
+    <div className=" flex grow flex-row flex-nowrap space-x-2 border-l-2 border-l-gray-200 px-2">
+      {tabs.map((tabName) => (
+        <Link
+          key={tabName}
+          to={router.Tab({ tab: tabName })}
+          className={clsx(
+            "my-auto rounded border-2 border-gray-200 p-1 text-sm text-gray-700 hover:bg-gray-200 hover:text-gray-900"
+          )}
+        >
+          {tabName}
+        </Link>
+      ))}
+    </div>
+  );
+};
--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -3,14 +3,18 @@ import { FC } from "react";
 import { Alliance } from "./Alliance";
 import { Connection } from "./Connected";
 import { SettingsDropdown } from "./SettingsDropdown";
+import { TabButtons } from "./TabButtons";
 
 export const TabBar: FC = () => {
   return (
-    <nav className="flex w-screen bg-white px-2 py-1 outline outline-1 outline-offset-0 outline-gray-100 backdrop-blur-lg">
-      <SettingsDropdown>
-        <Connection />
-      </SettingsDropdown>
-      <Alliance />
+    <nav className="flex h-[52px] w-screen bg-white px-2 py-1 outline outline-1 outline-offset-0 outline-gray-100 backdrop-blur-lg">
+      <div className=" w-[15.5rem] shrink-0">
+        <SettingsDropdown>
+          <Connection />
+        </SettingsDropdown>
+        <Alliance />
+      </div>
+      <TabButtons />
     </nav>
   );
 };
