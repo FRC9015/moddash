f2a5acef9b4a92793a2d1540fedecbee8ccf68b4
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-04-17 11:53:43 -0400

    default tab value

--- src/pages/Tab/index.tsx
+++ src/pages/Tab/index.tsx
@@ -1,11 +1,26 @@
+import { NetworkTablesTypeInfos } from "ntcore-ts-client";
 import { FC } from "react";
+
+import { useBaseMDProperties } from "@/utils/moddash/useBaseMDProperties";
+import useNTValue from "@/utils/nt/useNTValue";
 
 type Props = {
   tabId: string;
 };
 
 const Page: FC<Props> = () => {
-  return <div></div>;
+  const mode = useBaseMDProperties("mode");
+  const modedata = useNTValue(
+    "/moddash/Dashboard/mode/value",
+    NetworkTablesTypeInfos.kString,
+    "init"
+  );
+  return (
+    <div>
+      <div>{JSON.stringify(mode)}</div>
+      <div>{JSON.stringify(modedata)}</div>
+    </div>
+  );
 };
 
 export default Page;
