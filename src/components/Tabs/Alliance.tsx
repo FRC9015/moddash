eb95d13fc34c4731ad4e29ffc1698bc1cc598b8e
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-04-17 11:53:10 -0400

    display station number

--- src/components/Tabs/Alliance.tsx
+++ src/components/Tabs/Alliance.tsx
@@ -4,16 +4,16 @@ import { FC } from "react";
 import { useFMSValues } from "@/utils/nt/useFMSValues";
 
 export const Alliance: FC = () => {
-  const { isRedAlliance } = useFMSValues();
+  const { isRedAlliance, stationNumber } = useFMSValues();
   return (
-    <div className="m-1 inline-flex w-20 border-spacing-0 items-center justify-center rounded border-2 border-gray-200 p-1 text-sm">
+    <div className="m-1 inline-flex w-24 border-spacing-0 items-center justify-center rounded border-2 border-gray-200 p-1 text-sm">
       {isRedAlliance ? (
         <span className="inline-flex items-center font-medium text-[#ED1C24]">
-          <FlagIcon className="mr-2  h-6 w-auto " /> Red
+          <FlagIcon className="mr-2  h-6 w-auto " /> Red {stationNumber}
         </span>
       ) : (
         <span className="inline-flex items-center font-medium text-[#0066B3]">
-          <FlagIcon className="mr-2  h-6 w-auto " /> Blue
+          <FlagIcon className="mr-2  h-6 w-auto " /> Blue {stationNumber}
         </span>
       )}
     </div>
